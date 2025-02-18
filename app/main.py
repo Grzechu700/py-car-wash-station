4243a68e40d7e2a9d5f152fc571672d8b4bfc04a
Marcin <grzechnikmarcin@wp.pl>
2025-02-19 00:51:53 +0100

    dev-car-wash-station

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
-        self. clean_mark = clean_mark
+        self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -20,14 +27,14 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: str) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         cost = ((car.comfort_class
                 * (self.clean_power - car.clean_mark)
                 * self.average_rating)
                 / self.distance_from_city_center)
         return round(cost, 1)
 
-    def wash_single_car(self, car: str) -> int:
+    def wash_single_car(self, car: Car) -> int:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
